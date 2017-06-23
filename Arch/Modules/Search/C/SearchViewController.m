e6883068081b7bce0f418ea3365b85970d3782fd
dujianwei <dujianwei@cstonline.com>
2017-06-23 17:32:04 +0800

    add title

--- Arch/Modules/Search/C/SearchViewController.m
+++ Arch/Modules/Search/C/SearchViewController.m
@@ -25,24 +25,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.hidesBottomBarWhenPushed = YES;
-
+    self.title = @"搜索";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 - (IBAction)presentAction:(id)sender {
     
     [[UIBus sharedInstance] present:kSearchRoute];
